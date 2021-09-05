d51acce856f4aa6874fa29f1836ac60990acf9fd
Sanket2604 <umaymailme2604@gmail.com>
2021-09-05 12:18:59 +0530

    animation correction

--- js/events.js
+++ js/events.js
@@ -2,30 +2,26 @@ let tl = gsap.timeline({scrollTrigger:{
     trigger:".bg1",
     start:"top 100%",
 }})
-.from('.r1', { scale: 0 , opacity: 0, ease: 'power1', duration: 1})
 .from('.bg1', { rotateZ: "-30deg" , opacity: 0, ease: 'power1', duration: 1})
 .from('.c1', { x: '+50%', opacity: 0 , ease: 'power3', duration: 1})
 
 let t2 = gsap.timeline({scrollTrigger:{
     trigger:".bg2",
-    start:"top 80%",
+    start:"top 90%",
 }})
-.from('.r2', { scale: 0 , opacity: 0, ease: 'power1', duration: 1})
 .from('.bg2', { rotateZ: "+30deg" , opacity: 0, ease: 'power1', duration: 1})
 .from('.c2', { x: '-50%', opacity: 0 , ease: 'power3', duration: 1})
 
 let t3 = gsap.timeline({scrollTrigger:{
     trigger:".bg3",
-    start:"top 80%",
+    start:"top 90%",
 }})
-.from('.r3', { scale: 0 , opacity: 0, ease: 'power1', duration: 1})
 .from('.bg3', { rotateZ: "-30deg" , opacity: 0, ease: 'power1', duration: 1})
 .from('.c3', { x: '+50%', opacity: 0 , ease: 'power3', duration: 1})
 
 let t4 = gsap.timeline({scrollTrigger:{
     trigger:".bg4",
-    start:"top 80%",
+    start:"top 90%",
 }})
-.from('.r4', { scale: 0 , opacity: 0, ease: 'power1', duration: 1})
 .from('.bg4', { rotateZ: "+30deg" , opacity: 0, ease: 'power1', duration: 1})
 .from('.c4', { x: '-50%', opacity: 0 , ease: 'power3', duration: 1})
--- js/services.js
+++ js/services.js
@@ -4,10 +4,4 @@
         start:"top 50%",
     }})
     .from('.he1', { scale: 0, opacity: 0, ease: 'circ', delay: 1})
-
-    let t2 = gsap.timeline({scrollTrigger:{
-        trigger:".cont2",
-        start:"top 50%",
-    }})
-    .from('.he2', { scale: 0, opacity: 0, ease: 'circ', delay: 1})
     .from('.grid_ele', { opacity: 0, ease: 'power3', stagger: 0.5})
